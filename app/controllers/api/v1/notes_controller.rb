4d25bcb689b4d71403bcf956e8e409f989e0c9d0
Lois Lee <lois.lee@me.com>
2019-10-09 22:05:49 -0400

    add api endpoint for creating new note, sort by desc

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -1,8 +1,19 @@
 module Api::V1
   class NotesController < ApplicationController
     def index
-      @notes = Note.all
+      @notes = Note.order("created_at DESC")
       render json: @notes
     end
+
+    def create
+      @note = Note.create(note_params)
+      render json: @note
+    end
+        
+    private
+    
+      def note_params
+        params.require(:note).permit(:title, :body)
+      end
   end
 end
--- client/src/components/NotesContainer.js
+++ client/src/components/NotesContainer.js
@@ -20,11 +20,27 @@ class NotesContainer extends Component {
     .catch(error => console.log(error))
   }
 
+  addNewNote = () => {
+    axios.post(
+      'http://localhost:3001/api/v1/notes',
+      { note:
+        {
+          title: '',
+          body: ''
+        }
+      }
+    )
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => console.log(error))
+  }
+  
   render() {
     return (
       <div>
         <div>
-          <button className="newNoteButton">
+          <button className="newNoteButton" onClick={this.addNewNote} >
             New Note
           </button>
         </div>
