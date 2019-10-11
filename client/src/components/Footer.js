b6ccca44d4c0b50fc623f5b62cb96d3695a53eda
Lois Lee <lois.lee@me.com>
2019-10-11 16:36:10 -0400

    add three stateless components

---
+++ client/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+  return(
+    <footer>
+      <p>© Lois K. Lee 2019</p>
+      <ul className='footer'>
+        <li><a href="https://github.com/loisklee/notesy">Github</a></li>
+        <li><a href="http://loisklee.com">Portfolio</a></li>
+        <li><a href="https://www.linkedin.com/in/loisklee">LinkedIn</a></li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ client/src/components/Further.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Further= () => {
+
+  return(
+    <main>
+
+    <div class='para'>
+      <h3>  Notesy is a work in progress with more features soon to come:</h3>
+      <div class = 'para'> 
+        <li>user auth; users get their own boards</li>
+        <li>multiple boards via tabs</li>
+        <li>automatic date & time stamp functionality</li>
+      </div>
+    </div>
+
+    </main>
+  )
+}
+
+export default Further;
