9da9abf1548eb110f5df6ee0ded95be8ed4bd53f
Lois Lee <lois.lee@me.com>
2019-10-23 20:40:02 -0400

    add store, thunk, rootreducer

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,5 @@
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import About from './About'
 import NotesContainer from './NotesContainer'
 import Further from './Further'
--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom';
 import App from './App'
-import { createStore, applyMiddleware, compose } from 'redux'
-import noteReducer from './reducers/noteReducer'
-import thunk from 'redux-thunk'
+import store from './store';
 import { Provider } from 'react-redux'
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore (noteReducer, composeEnhancers(applyMiddleware(thunk)))
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <Provider store={store}>
